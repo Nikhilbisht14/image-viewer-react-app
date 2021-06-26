7c14c0042d91b32072b31318f9caee86de9e48f3
root <nikhilbisht837@gmail.com>
2021-06-26 21:14:28 +0530

    Fetch response from the instagram api

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -1,111 +1,138 @@
 import React, { Component } from "react";
 import Header from "../../common/header/Header";
 import "./Home.css";
-import Avatar from "@material-ui/core/Avatar";
-import Card from "@material-ui/core/Card";
 import { withStyles } from "@material-ui/core/styles";
 import GridList from "@material-ui/core/GridList";
 import GridListTile from "@material-ui/core/GridListTile";
-import CardHeader from "@material-ui/core/CardHeader";
-import CardContent from "@material-ui/core/CardContent";
-import Typography from "@material-ui/core/Typography";
-import LionelMessi from '../../assests/LionelMessi.jpg'
-import Divider from "@material-ui/core/Divider";
-import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder";
-import FormControl from "@material-ui/core/FormControl";
-import Input from "@material-ui/core/Input";
-import Button from "@material-ui/core/Button";
-import InputLabel from "@material-ui/core/InputLabel";
+import Post from '../home/post/Post';
+// import Avatar from "@material-ui/core/Avatar";
+// import Card from "@material-ui/core/Card";
+// import CardHeader from "@material-ui/core/CardHeader";
+// import CardContent from "@material-ui/core/CardContent";
+// import Typography from "@material-ui/core/Typography";
+// import LionelMessi from '../../assests/LionelMessi.jpg'
+// import Divider from "@material-ui/core/Divider";
+// import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder";
+// import FormControl from "@material-ui/core/FormControl";
+// import Input from "@material-ui/core/Input";
+// import Button from "@material-ui/core/Button";
+// import InputLabel from "@material-ui/core/InputLabel";
 
 
-const styles = (theme) => ({
-  root: {
-    flexGrow: 1,
-    backgroundColor: theme.palette.background.paper,
-  },
-  gridListMain: {
-    transform: "translateZ(0)",
-    width: "1265px",
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  formControl: {
-    margin: theme.spacing(),
-    minWidth: 240,
-    maxWidth: 240,
-  },
-  title: {
-    color: theme.palette.primary.light,
-  },
-});
+// const styles = (theme) => ({
+//   root: {
+//     flexGrow: 1,
+//     backgroundColor: theme.palette.background.paper,
+//   },
+//   gridListMain: {
+//     transform: "translateZ(0)",
+//     width: "1265px",
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//   },
+//   formControl: {
+//     margin: theme.spacing(),
+//     minWidth: 240,
+//     maxWidth: 240,
+//   },
+//   title: {
+//     color: theme.palette.primary.light,
+//   },
+// });
 
-const postStyle = {
-  hashtagStyle: {
-    display: "inline",
-    paddingRight: "2px",
-    fontSize: "15px",
-    color: "#5bbce4",
-  },
-  captionStyle: {
-    fontSize: "20px",
-    paddingTop: "10px",
-    color: "black",
-  },
-  likeIconStyle: {
-    fontSize: "40px",
-  },
-};
+// const postStyle = {
+//   hashtagStyle: {
+//     display: "inline",
+//     paddingRight: "2px",
+//     fontSize: "15px",
+//     color: "#5bbce4",
+//   },
+//   captionStyle: {
+//     fontSize: "20px",
+//     paddingTop: "10px",
+//     color: "black",
+//   },
+//   likeIconStyle: {
+//     fontSize: "40px",
+//   },
+// };
 
-const gridListTileStyle = {
-  width: "590px",
-  margin: "10px",
-};
+// const gridListTileStyle = {
+//   width: "590px",
+//   margin: "10px",
+// };
 
-const cardStyle = {
-  width: "80%",
-  height: "100%",
-};
+// const cardStyle = {
+//   width: "80%",
+//   height: "100%",
+// };
 
-const commentStyle = {
-  formControlStyle: {
-    width: "80%",
-    marginRight: "10px",
-    height: "40px",
-    marginTop: "60px",
-  },
-  commentButtonStyle: {
-    height: "45px",
-    marginTop: "-50px",
-    marginLeft: "470px",
-  },
-};
+// const commentStyle = {
+//   formControlStyle: {
+//     width: "80%",
+//     marginRight: "10px",
+//     height: "40px",
+//     marginTop: "60px",
+//   },
+//   commentButtonStyle: {
+//     height: "45px",
+//     marginTop: "-50px",
+//     marginLeft: "470px",
+//   },
+// };
 
-const tileData = [
+const postDetails = [
   {
+    id : 1,
     img: "../../assests/LionelMessi.jpg",
     content: 'content1',
     author: 'author1',
     likes: '10'
   },
   {
+    id : 2,
     img: "LionelMessi",
     content: 'content2',
     author: 'author2',
     likes: '20'
   },
   {
+    id : 3,
     img: "LionelMessi",
     content: 'content3',
     author: 'author3',
     likes: '30',
   },
   {
+    id : 4,
     img: "LionelMessi",
     content: 'content4',
     author: 'author4',
     likes: '40',
   }
 ];
+
+
+// New style themes
+
+const styles = theme => ({
+  gridContainer: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 90
+  },
+  gridList: {
+    width: 1100,
+    height: 'auto',
+    overflowY: 'auto'
+  },
+});
+
+const gridListTileStyle = {
+  width: "490px",
+  margin: "10px",
+};
 
 class Home extends Component {
 
@@ -136,55 +163,80 @@ class Home extends Component {
 
 
   render() {
-
     const { classes } = this.props;
     return (
       <div>
-        <Header title="Image Viewer" showHomePage="home" history={this.props.history}></Header>
-        <GridList cols={2} cellHeight={500} className={classes.gridListMain}>
-          {tileData.map((eg) => (
-            // Card header
-            <GridListTile cols={2} key="post1" style={gridListTileStyle}>
-              <Card style={{ cardStyle }} variant="outlined">
-                <CardHeader
-                  avatar={<Avatar aria-label="recipe" src={eg.img}></Avatar>}
-                  title="Messi"
-                  subheader="24/06/21 22:33:44"
-                ></CardHeader>
-
-                {/* Image, image-content and likes section */}
-                <CardContent>
-                  <img src={eg.img} alt="imag1" className="post-image" /><br /><br/>
-                  <Divider style={{ backgroundColor: "#c0c0c0" }} />
-                  <Typography variant="h5" style={postStyle.captionStyle}>
-                    {eg.content}
-                  </Typography>
-                  <div>
-                    <Typography display="inline" variant="caption" style={postStyle.hashtagStyle}>#Best #skills #Passion</Typography>
-                  </div>
-                  <div className="like-section">
-                    <FavoriteBorderIcon style={postStyle.likeIconStyle} />
-                    <span className="like-post">{eg.likes}</span>
-                  </div>
-
-                  {/* Comment section */}
-                  <div className="comment-section">
-                    <FormControl style={commentStyle.formControlStyle}>
-                      <InputLabel htmlFor="addComment">Add a comment</InputLabel>
-                      <Input id="addComment" type="text" placeholder="Add a comment"
-                      />
-                    </FormControl>
-                    <Button className="addBtn" variant="contained" color="primary" style={commentStyle.commentButtonStyle}>ADD</Button>
-                  </div>
-
-                </CardContent>
-              </Card>
-            </GridListTile>
-          ))}
-        </GridList>
+        <Header
+          history={this.props.history}
+          title="Image Viewer"
+          showHomePage="home"
+          searchHandler={this.searchTextHandler}
+        ></Header>
+        <div className={classes.gridContainer}>
+          <GridList cellHeight={'auto'} cols={3} className={classes.gridList}>
+            {this.state.postDetails.map((item, index) => (
+              <GridListTile key={item.id} style={gridListTileStyle}>
+                <Post detail={item} description={this.state.postDescription} />
+              </GridListTile>
+            ))}
+          </GridList>
+        </div>
       </div>
-    );
+    )
   }
 }
 
 export default withStyles(styles)(Home);
+//   render() {
+
+//     const { classes } = this.props;
+//     return (
+//       <div>
+//         <Header title="Image Viewer" showHomePage="home" history={this.props.history}></Header>
+//         <GridList cols={2} cellHeight={500} className={classes.gridListMain}>
+//           {tileData.map((eg) => (
+//             // Card header
+//             <GridListTile cols={2} key="post1" style={gridListTileStyle}>
+//               <Card style={{ cardStyle }} variant="outlined">
+//                 <CardHeader
+//                   avatar={<Avatar aria-label="recipe" src={eg.img}></Avatar>}
+//                   title="Messi"
+//                   subheader="24/06/21 22:33:44"
+//                 ></CardHeader>
+
+//                 {/* Image, image-content and likes section */}
+//                 <CardContent>
+//                   <img src={eg.img} alt="imag1" className="post-image" /><br /><br/>
+//                   <Divider style={{ backgroundColor: "#c0c0c0" }} />
+//                   <Typography variant="h5" style={postStyle.captionStyle}>
+//                     {eg.content}
+//                   </Typography>
+//                   <div>
+//                     <Typography display="inline" variant="caption" style={postStyle.hashtagStyle}>#Best #skills #Passion</Typography>
+//                   </div>
+//                   <div className="like-section">
+//                     <FavoriteBorderIcon style={postStyle.likeIconStyle} />
+//                     <span className="like-post">{eg.likes}</span>
+//                   </div>
+
+//                   {/* Comment section */}
+//                   <div className="comment-section">
+//                     <FormControl style={commentStyle.formControlStyle}>
+//                       <InputLabel htmlFor="addComment">Add a comment</InputLabel>
+//                       <Input id="addComment" type="text" placeholder="Add a comment"
+//                       />
+//                     </FormControl>
+//                     <Button className="addBtn" variant="contained" color="primary" style={commentStyle.commentButtonStyle}>ADD</Button>
+//                   </div>
+
+//                 </CardContent>
+//               </Card>
+//             </GridListTile>
+//           ))}
+//         </GridList>
+//       </div>
+//     );
+//   }
+// }
+
+// export default withStyles(styles)(Home);
